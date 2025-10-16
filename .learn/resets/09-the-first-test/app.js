fa26f5f62d4edcb83dbfe9dd7d926cbd7f5c7340
Richard <richg0923@gmail.com>
2025-10-16 01:00:05 +0000

    final jest test

---
+++ .learn/resets/09-the-first-test/app.js
@@ -0,0 +1,16 @@
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromDollarToYen =(dollar) => {
+    return (dollar/1.07)*156.5
+}
+const fromEuroToDollar =(Euro) => {
+    return Euro*1.07
+}
+const fromYenToPound =(yen) => {
+    return (yen/156.5)*0.87
+}
